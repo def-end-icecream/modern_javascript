6b8739ea6002ecabb7e0821eb9c3f791a779dbf0
dani <dani.zaghian@gmail.com>
2021-02-08 10:03:48 -0800

    npm init and moment library installed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Hello from JavaScript!");
+var startOfDay = moment().startOf('day').fromNow();
+var todayFormatted = moment().format("MMM Do YY");
+console.log(startOfDay);
+console.log(todayFormatted);
