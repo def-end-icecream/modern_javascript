34f50c359ed94ce6f6e04693c9f660419a7c7aa2
dani <dani.zaghian@gmail.com>
2021-02-08 11:16:18 -0800

    adds module bundler dependency

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log("Hello from JavaScript!");
 var startOfDay = moment().startOf('day').fromNow();
 var todayFormatted = moment().format("MMM Do YY");
 console.log(startOfDay);
 console.log(todayFormatted);
+console.log(math.sqrt(-4));
